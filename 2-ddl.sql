33050e39dd56d0f2e0d5a58051484fc5c5ea161f
Karl255 <49595438+Karl255@users.noreply.github.com>
2026-01-26 23:41:26 +0100

    #6 add schemas, slight reorg

--- 2-ddl.sql
+++ 2-ddl.sql
@@ -1,66 +1,64 @@
 -- Tables
 
-CREATE TABLE Organisation (
+CREATE TABLE Sifrarnik.Organisation (
     id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
 	INDEX IX_name (name)
 );
 
-CREATE TABLE "Group" (
+CREATE TABLE Sifrarnik."Group" (
     id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     -- 'ALL', 'USER', 'CUSTOM'
 	-- samo CUSTOM se može brisati
     type VARCHAR(6) NOT NULL,
     organisationId INT NOT NULL,
-    FOREIGN KEY (organisationId) REFERENCES Organisation(id)
+    FOREIGN KEY (organisationId) REFERENCES Sifrarnik.Organisation(id)
 );
 
-CREATE TABLE "User" (
+CREATE TABLE Sifrarnik."User" (
     id INT PRIMARY KEY,
     username VARCHAR(32) NOT NULL,
     -- SHA3-224
     passwordHash VARCHAR(28) NOT NULL,
-    organisationId INT NOT NULL,
-    FOREIGN KEY (organisationId) REFERENCES Organisation(id),
 	INDEX IX_username (username)
 );
 
-CREATE TABLE GroupUser (
+CREATE TABLE Sifrarnik.GroupUser (
 	userId INT NOT NULL,
 	groupId INT NOT NULL,
 	PRIMARY KEY (userId, groupId),
-	FOREIGN KEY (userId) REFERENCES "User"(id),
-	FOREIGN KEY (groupId) REFERENCES "Group"(id)
+	FOREIGN KEY (userId) REFERENCES Sifrarnik."User"(id),
+	FOREIGN KEY (groupId) REFERENCES Sifrarnik."Group"(id)
 )
 
-CREATE TABLE Folder (
+CREATE TABLE Io.Folder (
     id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
 	parentFolderId INT NULL,
-	FOREIGN KEY (parentFolderId) REFERENCES Folder(id)
+	FOREIGN KEY (parentFolderId) REFERENCES Io.Folder(id)
 );
 
-CREATE TABLE Permission (
+CREATE TABLE Io.Permission (
     groupId INT NOT NULL,
     folderId INT NOT NULL,
     -- 'READ', 'EDIT', 'MANAGE'
     level VARCHAR(6) NOT NULL,
 	PRIMARY KEY (groupId, folderId),
-	FOREIGN KEY (groupId) REFERENCES "Group"(id),
-	FOREIGN KEY (folderId) REFERENCES Folder(id)
+	FOREIGN KEY (groupId) REFERENCES Sifrarnik."Group"(id),
+	FOREIGN KEY (folderId) REFERENCES Io.Folder(id)
 );
 
-CREATE TABLE Note (
+CREATE TABLE Io.Note (
     id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
 	content VARCHAR(MAX) NOT NULL,
 	folderId INT NOT NULL,
-	FOREIGN KEY (folderId) REFERENCES Folder(id),
+	FOREIGN KEY (folderId) REFERENCES Io.Folder(id),
 	UNIQUE NONCLUSTERED (name, folderId)
 );
 
-CREATE TABLE Reminder (
+CREATE TABLE Io.Reminder (
     groupId INT NOT NULL,
     noteId INT NOT NULL,
     
@@ -68,32 +66,32 @@ CREATE TABLE Reminder (
     -- možda još dodati repeat?
     
     PRIMARY KEY (groupId, noteId),
-    FOREIGN KEY (groupId) REFERENCES "Group"(id),
-    FOREIGN KEY (noteId) REFERENCES Note(id)
+    FOREIGN KEY (groupId) REFERENCES Sifrarnik."Group"(id),
+    FOREIGN KEY (noteId) REFERENCES Io.Note(id)
 );
 
-CREATE TABLE LoggedRemidner (
+CREATE TABLE Io.LoggedRemidner (
 	id INT PRIMARY KEY,
     groupId INT NOT NULL,
     noteId INT NOT NULL,
 	fired DATETIME NOT NULL,
-	FOREIGN KEY (groupId, noteId) REFERENCES Reminder(groupId, noteId)
+	FOREIGN KEY (groupId, noteId) REFERENCES Io.Reminder(groupId, noteId)
 )
 
-CREATE TABLE Tag (
+CREATE TABLE Io.Tag (
     id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     organisationId INT NOT NULL,
-    FOREIGN KEY (organisationId) REFERENCES Organisation(id),
+    FOREIGN KEY (organisationId) REFERENCES Sifrarnik.Organisation(id),
 	UNIQUE NONCLUSTERED (name, organisationId)
 );
 
-CREATE TABLE TaggedNote (
+CREATE TABLE Io.TaggedNote (
     noteId INT NOT NULL,
     tagId INT NOT NULL,
     PRIMARY KEY (noteId, tagId),
-    FOREIGN KEY (noteId) REFERENCES Note(id),
-    FOREIGN KEY (tagId) REFERENCES Tag(id)
+    FOREIGN KEY (noteId) REFERENCES Io.Note(id),
+    FOREIGN KEY (tagId) REFERENCES Io.Tag(id)
 );
 
 -- Triggers
